8acb3d0a72cd7b33ce9f8463783522a72423e9d1
Salavat Nursafin <krowlexing@gmail.com>
2024-05-05 11:41:44 +0400

    add virtualized tables

--- src/components/GenericTable.tsx
+++ src/components/GenericTable.tsx
@@ -4,11 +4,14 @@ import {
     Table,
     TableBody,
     TableCell,
+    TableContainer,
     TableHead,
     TableRow,
 } from "@mui/material";
 import { Ed807Metadata } from "../data/Ed807";
-import { ReactNode } from "react";
+import { ReactNode, useMemo } from "react";
+import { TableComponents, TableVirtuoso, Virtuoso } from "react-virtuoso";
+import React from "react";
 
 interface Props<T> {
     description: TableDescription<T>;
@@ -26,11 +29,36 @@ export function GenericTable<T>(props: Props<T>) {
     const body = generateBody(description, values, onRowClick);
 
     return (
-        <Table sx={sx}>
-            {header}
-            {body}
-        </Table>
+        <>
+            <TableVirtuoso
+                style={{ height: "900px" }}
+                data={values}
+                components={components(onRowClick)}
+                fixedHeaderContent={() =>
+                    generateHeader(description, onColumnHeaderClick)
+                }
+                itemContent={(_, entry) => rowContent(description, entry)}
+            />
+        </>
     );
+}
+
+function rowContent<T>(description: TableDescription<T>, value: T) {
+    const keys = Object.keys(description) as (keyof TableDescription<T>)[];
+
+    const cells = keys.map(key => (
+        <TableCell>{value[key] as ReactNode}</TableCell>
+    ));
+
+    return <>{cells}</>;
+}
+
+function nativeRowContent<T>(description: TableDescription<T>, value: T) {
+    const keys = Object.keys(description) as (keyof TableDescription<T>)[];
+
+    const cells = keys.map(key => <td>{value[key] as ReactNode}</td>);
+
+    return <>{cells}</>;
 }
 
 function generateHeader<T>(
@@ -45,11 +73,58 @@ function generateHeader<T>(
         </TableCell>
     ));
 
-    return (
-        <TableHead>
-            <TableRow>{cells}</TableRow>
-        </TableHead>
-    );
+    return <TableRow>{cells}</TableRow>;
+}
+
+function generateNativeHeader<T>(
+    description: TableDescription<T>,
+    onColumnClicked?: (key: keyof TableDescription<T>) => void,
+) {
+    const keys = Object.keys(description) as (keyof TableDescription<T>)[];
+
+    const cells = keys.map(key => (
+        <td onClick={() => onColumnClicked && onColumnClicked(key)}>
+            <b>{description[key]}</b>
+        </td>
+    ));
+
+    return <tr>{cells}</tr>;
+}
+
+function components<T>(onRowClick: (item: T) => void): TableComponents<T> {
+    return {
+        Scroller: React.forwardRef<HTMLDivElement>((props, ref) => (
+            <TableContainer component={Paper} {...props} ref={ref} />
+        )),
+        Table: props => (
+            <Table
+                {...props}
+                sx={{
+                    borderCollapse: "separate",
+                    tableLayout: "fixed",
+                }}
+            />
+        ),
+        TableHead: props => (
+            // @ts-expect-error Virtuoso's props and TableHead's props are not exact match
+            <TableHead {...props} sx={{ background: "white" }} />
+        ),
+        TableRow: ({ item, ...props }) => (
+            <TableRow
+                {...props}
+                onClick={() => onRowClick(item)}
+                sx={{
+                    transition: "all 0.1s linear",
+                    ":hover": {
+                        backgroundColor: "gray",
+                    },
+                }}
+            />
+        ),
+        TableBody: React.forwardRef<HTMLTableSectionElement>((props, ref) => (
+            <TableBody {...props} ref={ref} />
+        )),
+    };
 }
 
 function generateBody<T>(
